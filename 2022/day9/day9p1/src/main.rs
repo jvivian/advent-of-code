76e9a71d204defb10f9a9ebbd5275ed37b76b1b0
John Vivian <jtvivian@gmail.com>
2023-01-08 23:34:33 -0800

    Day9p1 complete

--- 2022/day9/day9p1/src/main.rs
+++ 2022/day9/day9p1/src/main.rs
@@ -1,8 +1,9 @@
+use aoclib::get_lines;
 use std::collections::HashSet;
 use std::num::ParseIntError;
-use std::path::PathBuf;
 use std::str::FromStr;
 use thiserror::Error;
+use type_cli::CLI;
 /// Advent of Code
 /// Day 9 - Part 1
 /// Author: John Vivian
@@ -25,12 +26,21 @@ use thiserror::Error;
 /// -- AlignedGap => Move_Cardinal
 /// -- UnalignedGap => Move_Diagonal_Adjacent
 
+#[derive(CLI)]
+struct Input(String);
+
 #[derive(Debug, PartialEq)]
-enum Move {
-    Up(i32),
-    Down(i32),
-    Left(i32),
-    Right(i32),
+struct Move {
+    n: i32,
+    dir: Direction,
+}
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 #[derive(Error, Debug, PartialEq)]
@@ -40,7 +50,7 @@ enum ParseMoveError {
     #[error("Failed to parse number of moves")]
     IntError(#[from] ParseIntError),
     #[error("Failed to parse direction {0}")]
-    DirError(String),
+    InvalidDirection(String),
 }
 
 impl FromStr for Move {
@@ -50,61 +60,195 @@ impl FromStr for Move {
             .split_once(' ')
             .ok_or(ParseMoveError::InvalidInput(s.into()))?;
         match d {
-            "U" => Ok(Move::Up(n.parse()?)),
-            "D" => Ok(Move::Down(n.parse()?)),
-            "L" => Ok(Move::Left(n.parse()?)),
-            "R" => Ok(Move::Right(n.parse()?)),
-            _ => Err(ParseMoveError::DirError(d.into())),
+            "U" => Ok(Move {
+                n: n.parse()?,
+                dir: Direction::Up,
+            }),
+            "D" => Ok(Move {
+                n: n.parse()?,
+                dir: Direction::Down,
+            }),
+            "L" => Ok(Move {
+                n: n.parse()?,
+                dir: Direction::Left,
+            }),
+            "R" => Ok(Move {
+                n: n.parse()?,
+                dir: Direction::Right,
+            }),
+            _ => Err(ParseMoveError::InvalidDirection(d.into())),
         }
     }
 }
 
-struct Coord(i32, i32);
+// State of the rope after a Move
+#[derive(PartialEq, Debug)]
+enum State {
+    Adjacent,
+    CardinalGap,
+    DiagonalGap,
+}
 
-impl Coord {
-    fn _move(self, m: Move) -> Self {
-        match m {
-            Move::Up(n) => Coord(self.0, self.1 + n),
-            Move::Down(n) => Coord(self.0, self.1 - n),
-            Move::Left(n) => Coord(self.0 - n, self.1),
-            Move::Right(n) => Coord(self.0 + n, self.1),
+#[derive(Debug, PartialEq)]
+struct StateError;
+
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+struct Knot(i32, i32);
+
+impl Knot {
+    // Euclidean distance
+    fn distance(&self, k: &Knot) -> f32 {
+        (((self.0 - k.0) as f32).powf(2.0) + ((self.1 - k.1) as f32).powf(2.0)).sqrt()
+    }
+
+    // Step the knot in a given direction
+    fn step(self, dir: &Direction) -> Self {
+        match dir {
+            Direction::Up => Knot(self.0, self.1 + 1),
+            Direction::Down => Knot(self.0, self.1 - 1),
+            Direction::Left => Knot(self.0 - 1, self.1),
+            Direction::Right => Knot(self.0 + 1, self.1),
         }
     }
 }
 
+#[derive(Debug)]
 struct Rope {
-    head: Coord,
-    tail: Coord,
+    head: Knot,
+    tail: Knot,
     // Store Tail positions
-    tpos: HashSet<Coord>,
+    tset: HashSet<Knot>,
 }
 
 impl Rope {
-    // Move Rope according to input
-    fn move_head(self, m: Move) -> Self {}
+    fn new() -> Self {
+        Rope {
+            head: Knot(0, 0),
+            tail: Knot(0, 0),
+            tset: HashSet::new(),
+        }
+    }
+    // Determine what state the rope is in
+    fn get_state(&self) -> Result<State, StateError> {
+        match self.head.distance(&self.tail) {
+            d if d < 2.0 => Ok(State::Adjacent),
+            d if d == 2.0 => Ok(State::CardinalGap),
+            d if d > 2.0 => Ok(State::DiagonalGap),
+            _ => Err(StateError),
+        }
+    }
+    // Move rope given an input such as `R 4`
+    fn move_rope(&mut self, m: &Move) {
+        for _ in 0..m.n {
+            self.head = self.head.step(&m.dir);
+            match self.get_state().unwrap() {
+                State::Adjacent => {}
+                State::CardinalGap => self.move_tail_cardinal(),
+                State::DiagonalGap => self.move_tail_diagonal(),
+            }
+            self.tset.insert(self.tail);
+        }
+    }
+
+    // Move tail cardinally adjacent to head
+    // Ex: T = (0, 0), H = (2, 0)
+    fn move_tail_cardinal(&mut self) {
+        if self.head.0 == self.tail.0 {
+            self.tail.1 = (self.tail.1 + self.head.1) / 2;
+        } else {
+            self.tail.0 = (self.tail.0 + self.head.0) / 2;
+        }
+    }
+
+    // Move tail diagonally adjacent to head
+    // Ex: T = (0, 0), H = (1, 2), newT = (1, 1)
+    fn move_tail_diagonal(&mut self) {
+        if (self.tail.0 - self.head.0).abs() == 1 {
+            self.tail.0 = self.head.0;
+            self.tail.1 = (self.tail.1 + self.head.1) / 2;
+        } else {
+            self.tail.1 = self.head.1;
+            self.tail.0 = (self.tail.0 + self.head.0) / 2;
+        }
+    }
+
+    // Knots traverse the Rope based on an input list
+    fn traverse(mut self, path: &str) -> Result<Self, ParseMoveError> {
+        for line in get_lines(path).map(|x| x.unwrap()).collect::<Vec<String>>() {
+            self.move_rope(&Move::from_str(&line)?)
+        }
+        Ok(self)
+    }
 }
 
-// enum TailAction {
-//     Overlap,
-//     Adjacent,
-//     AlignedGap,
-//     UnalignedGap,
-// }
-
 fn main() {
-    println!("Hello, world!");
+    let Input(path) = Input::process();
+    let rope = Rope::new().traverse(&path).expect("Failed traversing rope");
+    println!(
+        "--- Rope ---\nHead: {:?}\nTail: {:?}\nNumtail: {}",
+        rope.head,
+        rope.tail,
+        rope.tset.len()
+    );
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::path::PathBuf;
 
     fn test_path() -> PathBuf {
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test.txt")
     }
 
     #[test]
+    fn test_rope_traverse() {
+        let r = Rope::new().traverse(test_path().to_str().unwrap()).unwrap();
+        println!("{:?}", r);
+    }
+
+    #[test]
+    fn test_rope_move() {
+        let mut r = Rope::new();
+        r.move_rope(&Move {
+            n: 5,
+            dir: Direction::Right,
+        });
+        println!("{:?}", r);
+    }
+
+    #[test]
+    fn test_rope_get_state() {
+        let mut r = Rope::new();
+        assert_eq!(r.get_state(), Ok(State::Adjacent));
+        r.head = Knot(2, 0);
+        assert_eq!(r.get_state(), Ok(State::CardinalGap));
+        r.head = Knot(2, 1);
+        assert_eq!(r.get_state(), Ok(State::DiagonalGap));
+    }
+
+    #[test]
+    fn test_knot_distance() {
+        let k0 = Knot(0, 0);
+        let ka1 = Knot(1, 0);
+        let ka2 = Knot(-1, -1);
+        let kna1 = Knot(2, 0);
+        let knad1 = Knot(1, -2);
+        assert_eq!(k0.distance(&k0), 0.0);
+        assert_eq!(k0.distance(&ka1), 1.0);
+        assert_eq!(k0.distance(&ka2).round(), 1.0);
+        assert_eq!(k0.distance(&kna1), 2.0);
+        assert_eq!(k0.distance(&knad1).round(), 2.0);
+    }
+
+    #[test]
     fn test_coord_parse() {
-        assert_eq!(Move::from_str("R 4"), Ok(Move::Right(4)));
+        assert_eq!(
+            Move::from_str("R 4"),
+            Ok(Move {
+                n: 4,
+                dir: Direction::Right
+            })
+        );
     }
 }
